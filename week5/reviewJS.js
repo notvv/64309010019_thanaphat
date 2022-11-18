c86e3a451ec371adedf048b8e5c05a0a0ce71fed
notvv <newnot000@gmail.com>
2022-11-18 10:16:41 +0700

    Create reviewJS.js

---
+++ week5/reviewJS.js
@@ -0,0 +1,11 @@
+x = 5;
+var y = 10;
+let myName = "TNK";
+const year = 2022;
+//year = 2023;
+
+console.log("y เแ็นชนิด",typeof(y));
+console.log("myName เป็นชนิด",typeof(myName));
+
+y="JAVA";
+console.log(y,typeof(y));
